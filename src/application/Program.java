68717bb82679a6e99c3469dbf418f44a0c69af16
Vinicius Thiago <vtls@cin.ufpe.br>
2020-02-28 20:36:11 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,8 +24,9 @@ public class Program {
 		
 		list.forEach(System.out::println);
 		
-		
-		
+		System.out.println("\n=== TEST 3: seller findAll ====");
+		list = sellerDao.findAll();
+		list.forEach(System.out::println);
 	}
 
 }
